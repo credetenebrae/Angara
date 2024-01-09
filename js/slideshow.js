b425a5e0760da555c807eca4519a8d328d8ff9c5
unknown <322vocaloid@gmail.com>
2024-01-09 09:02:40 +0600

    first commit

---
+++ js/slideshow.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slideshow img');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Интервал времени между каждым слайдом
